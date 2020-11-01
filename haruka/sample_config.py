56a98ce432c1846bafaa779ab7637ef56b0cc88a
TECHYMAHESHYT <57401105+TECHYMAHESHYT@users.noreply.github.com>
2020-11-01 19:08:22 +0530

    Update sample_config.py

--- haruka/sample_config.py
+++ haruka/sample_config.py
@@ -10,13 +10,13 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "YOUR KEY HERE"
-    OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "YOUR USERNAME HERE"
+    API_KEY = "1072191977:AAGmIYoHDNpLSJldWPwHJfhM6_3NgScAfnY"
+    OWNER_ID = "990373740"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "@Techyyt"
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
-    MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
+    MESSAGE_DUMP = -492196044  # needed to make sure 'save from' messages persist
     LOAD = []
     NO_LOAD = ['translation', 'sed']
     WEBHOOK = False
@@ -25,16 +25,16 @@ class Config(object):
     # OPTIONAL
     SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
     SUPPORT_USERS = []  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    WHITELIST_USERS = [990373740]  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
     MAPS_API = ''
     CERT_PATH = None
     PORT = 5000
     DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
     STRICT_ANTISPAM = False
     WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
-    BAN_STICKER = 'CAADAgADOwADPPEcAXkko5EB3YGYAg'  # banhammer marie sticker
-    STRICT_GBAN = False
-    STRICT_GMUTE = False
+    BAN_STICKER = 'CAACAgUAAxkBAAEGniBfnrZCDyT6XTIGz3Lz0cYGJI7i6AACjQAD9ZpjI1PKToWHaZrHHgQ'  # banhammer marie sticker
+    STRICT_GBAN = True
+    STRICT_GMUTE = True
     ALLOW_EXCL = True  # Allow ! commands as well as /
     API_OPENWEATHER = None # OpenWeather API
 
